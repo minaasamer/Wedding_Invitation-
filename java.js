e9cae445d2ea5491030302d857064ee1549c8177
Mina Samer <minaevano312@gmail.com>
2026-08-22 20:21:38 +0200

    fix: increase auto scroll speed

--- java.js
+++ java.js
@@ -724,7 +724,7 @@ async function capturePhoto() {
 window.addEventListener("load", () => {
 
     const delay = 5000;
-    const duration = 47000;
+    const duration = 30000;
 
     let autoScrolling = false;
     let animationFrame = null;
